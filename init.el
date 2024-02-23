3e119a535c3b1498d2d6005ed99e49be2e94a540
Shrish Deshpande <shrish.deshpande@icloud.com>
2024-02-23 20:27:40 +0530

    Initial commit

---
+++ init.el
@@ -0,0 +1,47 @@
+(load "~/.emacs.d/packages")
+
+(use-package lsp-sourcekit
+  :after lsp-mode
+  :config
+  (setq lsp-sourcekit-executable "/usr/bin/sourcekit-lsp"))
+
+(use-package lsp-mode
+  :init
+  (setq lsp-keymap-prefix "C-l")
+  :hook ((lsp-mode . lsp-enable-which-key-integration))
+  :commands lsp)
+
+(load "~/.emacs.d/treemacs")
+(load-theme 'timu-macos t)
+
+(use-package company)
+(use-package lsp-ui
+  :after lsp-mode
+  :commands lsp-ui-mode
+  :hook (lsp-mode . lsp-ui-mode)
+  :config
+  (setq lsp-ui-sideline-enable t
+        lsp-ui-sideline-show-diagnostics t
+        lsp-ui-sideline-show-hover t
+        lsp-ui-sideline-show-code-actions t
+        lsp-ui-sideline-update-mode 'line))
+(use-package lsp-treemacs :commands lsp-treemacs-errors-list)
+
+(add-hook 'swift-mode-hook #'lsp)
+
+(use-package which-key
+    :config
+    (which-key-mode))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(treemacs-tab-bar treemacs-persp treemacs-magit treemacs-projectile treemacs-icons-dired lsp-ui lsp-treemacs lsp-sourcekit flycheck company)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
---
+++ packages.el
@@ -0,0 +1,29 @@
+(require 'package)
+
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+
+; list the packages you want
+(setq package-list
+  '(
+    use-package
+    flycheck
+    treemacs
+    lsp-mode
+    lsp-sourcekit
+    lsp-treemacs
+    lsp-ui
+    company
+    which-key
+    timu-macos-theme
+    swift-mode
+  )
+)
+
+
+; activate all the packages
+; (package-initialize)
+
+; install the missing packages
+(dolist (package package-list)
+  (unless (package-installed-p package)
+    (package-install package)))
